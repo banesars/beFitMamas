0b3f555e87e5688c01f5f60974c58c6b4b56267e
Shawn <70689023+Merchxnt@users.noreply.github.com>
2023-07-07 13:45:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessageReactions,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+    console.log(message.content);
+    if (message.content === 'hello')
+    message.reply('Hey! Welcome to BeFitMamas Community Server.');
+})
+
+client.login('MTEyNjkzNDE4MzEwOTk4NDMyNw.GI_loh.Yr5aZOK-yg1wgSFMtMUtcePgzLuzyc9bGRUJBw');
